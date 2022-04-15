fbfec5519ee4fdcf0182179b80ad1fa2d31134cb
Tsara Sudrajat <tsara.tf@gmail.com>
2022-04-15 12:32:53 +0700

    Remove unused bootstrap js

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -126,12 +126,6 @@ function Checkout() {
           </div>
         </div>
       </section>
-
-      <script
-        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
-        crossOrigin="anonymous"
-      ></script>
     </>
   )
 }
