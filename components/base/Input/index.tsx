056f798907dd6b8859cc907852b5d1c55cf620c0
Tsara Fatma Larasati Sudrajat <tsara.tf@gmail.com>
2022-04-25 23:41:36 +0700

    Refactor form inputs to base components, add eslint rule

---
+++ components/base/Input/index.tsx
@@ -0,0 +1,32 @@
+interface InputProps {
+  labelText: string;
+  label: string;
+  placeholder?: string;
+  inputType?: "text" | "tel";
+}
+
+function Input(props: InputProps) {
+  const { labelText, label, placeholder = "", inputType = "text", ...nativeProps } = props;
+
+  return (
+    <>
+      <label
+        htmlFor={label}
+        className="form-label text-lg fw-medium color-palette-1 mb-10"
+      >
+        {labelText}
+      </label>
+      <input
+        type={inputType}
+        className="form-control rounded-pill text-lg"
+        name={label}
+        id={label}
+        aria-describedby={label}
+        placeholder={placeholder}
+        {...nativeProps}
+      />
+    </>
+  );
+}
+
+export default Input;
--- pages/member/profile/edit.tsx
+++ pages/member/profile/edit.tsx
@@ -1,4 +1,5 @@
 import Sidebar from "../../../components/sections/Sidebar";
+import Input from "../../../components/base/Input";
 
 function ProfileEdit() {
   return (
@@ -24,22 +25,14 @@ function ProfileEdit() {
                     <input id="avatar" type="file" name="avatar" accept="image/png, image/jpeg" />
                   </div>
                 </div>
-                <div className="pt-30">
-                  <label htmlFor="name" className="form-label text-lg fw-medium color-palette-1 mb-10">Full
-                    Name</label>
-                  <input type="text" className="form-control rounded-pill text-lg" id="name" name="name" aria-describedby="name"
-                    placeholder="Enter your name"/>
+               <div className="pt-30">
+                  <Input label="name" labelText="Full name" placeholder="Enter your full name" />
                 </div>
                 <div className="pt-30">
-                  <label htmlFor="email" className="form-label text-lg fw-medium color-palette-1 mb-10">Email
-                    Address</label>
-                  <input type="email" className="form-control rounded-pill text-lg" id="email" name="email"
-                    aria-describedby="email" placeholder="Enter your email address"/>
+                  <Input label="email" labelText="Email" placeholder="Enter your email address" />
                 </div>
                 <div className="pt-30">
-                  <label htmlFor="phone" className="form-label text-lg fw-medium color-palette-1 mb-10">Phone</label>
-                  <input type="tel" className="form-control rounded-pill text-lg" id="phone" name="phone" aria-describedby="phone"
-                    placeholder="Enter your phone number"/>
+                  <Input label="phone" labelText="Phone Number" placeholder="+6228382828" inputType="tel" />
                 </div>
                 <div className="button-group d-flex flex-column pt-50">
                   <button type="submit" className="btn btn-save fw-medium text-lg text-white rounded-pill" role="button">Save My
