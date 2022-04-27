4a62707715aad80f155db80db70646591ec74214
Tsara Fatma Larasati Sudrajat <tsara.tf@gmail.com>
2022-04-27 13:34:06 +0700

    Format code

--- components/sections/CheckoutItemDetails/index.tsx
+++ components/sections/CheckoutItemDetails/index.tsx
@@ -6,30 +6,12 @@ function CheckoutItemDetails() {
       <h2 className="fw-bold text-xl color-palette-1 mb-20">
         Purchase Details
       </h2>
-      <DetailsRow
-        title="Your Game ID"
-        content="masayoshizero"
-      />
-      <DetailsRow
-        title="Order ID"
-        content="#GG001"
-      />
-      <DetailsRow
-        title="Item"
-        content="250 Diamonds"
-      />
-      <DetailsRow
-        title="Price"
-        content="Rp 42.280.500"
-      />
-      <DetailsRow
-        title="Tax (10%)"
-        content="Rp 4.228.000"
-      />
-      <DetailsRow
-        title="Total"
-        content="Rp 55.000.600"
-      />
+      <DetailsRow title="Your Game ID" content="masayoshizero" />
+      <DetailsRow title="Order ID" content="#GG001" />
+      <DetailsRow title="Item" content="250 Diamonds" />
+      <DetailsRow title="Price" content="Rp 42.280.500" />
+      <DetailsRow title="Tax (10%)" content="Rp 4.228.000" />
+      <DetailsRow title="Total" content="Rp 55.000.600" />
     </div>
   );
 }
--- components/sections/CheckoutPaymentDetails/index.tsx
+++ components/sections/CheckoutPaymentDetails/index.tsx
@@ -6,16 +6,10 @@ function CheckoutPaymentDetails() {
       <h2 className="fw-bold text-xl color-palette-1 mb-20">
         Payment Informations
       </h2>
-      <DetailsRow
-        title="Your Account Name"
-        content="Masayoshi Angga Zero"
-      />
+      <DetailsRow title="Your Account Name" content="Masayoshi Angga Zero" />
       <DetailsRow title="Type" content="Worldwide Transfer" />
       <DetailsRow title="Bank Name" content="Mandiri" />
-      <DetailsRow
-        title="Bank Account Name"
-        content="PT Store GG Indonesia"
-      />
+      <DetailsRow title="Bank Account Name" content="PT Store GG Indonesia" />
       <DetailsRow title="Bank Number" content="1800 - 9090 - 2021" />
     </div>
   );
